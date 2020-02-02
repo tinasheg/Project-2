f80a1a2b77f1e527de21011d99a67668e437432c
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-02-01 16:48:34 -0800

    adds handlebar files & updates htmlRoutes file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,10 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
+  });
+  app.get("/play", function(req, res) {
+    res.render("play");
   });
 
   // Load example page and pass in an example by id
