d5cd9471f19879c51f73377077906ec07b513d54
tinashe <giyavha_tinashe@berkeley.edu>
2020-02-06 20:22:52 -0800

    adds translation to htmlRoutes + example for use in play.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,44 @@
+/* eslint-disable prettier/prettier */
+require("dotenv").config();
 var db = require("../models");
+var Sequelize = require("sequelize");
+var googleTranslate = require("google-translate")(
+  process.env.GOOGLE_TRANSLATE_API
+);
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index");
   });
-  app.get("/play", function(req, res) {
-    res.render("play");
+  // eslint-disable-next-line prettier/prettier
+  app.get("/play", function (req, res) {
+    db.Dictionary.findOne({
+      where: {
+        level: "1"
+      },
+      order: Sequelize.literal("rand()")
+    }).then(function (result) {
+      var word = result.word;
+      var targetLanguage = "de";
+      try {
+        googleTranslate.translate(word, targetLanguage, function (
+          err,
+          translation
+        ) {
+          if (err) {
+            console.log("err1", err);
+          }
+          console.log("translated:", translation.translatedText);
+          res.render("play", {
+            originalWord: word,
+            translatedWord: translation.translatedText
+          });
+        });
+      } catch (err) {
+        console.log("err2", err);
+      }
+    });
   });
 
   // // Load example page and pass in an example by id
